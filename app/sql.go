d4fd1f9f6a6d44a78ca25d3f3cf412c497909b5d
kimulaco <kmrkdev@gmail.com>
2022-08-20 06:28:00 +0900

    feat(app): Add select query

--- app/sql.go
+++ app/sql.go
@@ -3,7 +3,45 @@ package app
 import (
 	"database/sql"
 	"log"
+	"strconv"
 )
+
+type SelectConfig struct {
+	Limit int
+}
+
+func (c SelectConfig) Init() SelectConfig {
+	if c.Limit <= 0 {
+		c.Limit = 20
+	}
+	return c
+}
+
+func SelectAppsWhereReleasedAt(
+	db *sql.DB,
+	releasedAt string,
+	c SelectConfig,
+) ([]App, error) {
+	c = c.Init()
+	rows, err := db.Query(`
+	SELECT
+		id, url, title, thumbnail_url, released_at, price, sale_price
+	FROM apps
+	WHERE
+		released_at = ?
+	LIMIT ?
+	`, releasedAt, strconv.Itoa(c.Limit))
+	if err != nil {
+		return []App{}, err
+	}
+
+	apps, err := CreateFromSqlRows(rows)
+	if err != nil {
+		return apps, err
+	}
+
+	return apps, nil
+}
 
 func CreateFromSqlRows(rows *sql.Rows) ([]App, error) {
 	apps := make([]App, 0)
